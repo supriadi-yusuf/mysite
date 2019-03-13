f16e4dbd6c1fe0b924d873d8a8f6e26a5b18eeed
supriadi-yusuf <supriadi96@gmail.com>
2019-03-13 09:35:15 +0700

    modify index view

--- polls/views.py
+++ polls/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello world! You are in polls's index")
+    latest_questions = models.Question.objects.order_by('-pub_date')[:5]
+    output = ", ".join([q.question_text for q in latest_questions])
+    return HttpResponse(output)
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
