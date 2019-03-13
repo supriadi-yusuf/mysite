ca28e8e0c845e51c17e78f42f5430ab67cc9cd9f
supriadi-yusuf <supriadi96@gmail.com>
2019-03-13 08:29:15 +0700

    add detail

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name="index"),
+  url(r'^(?P<question_id>\d+)/$', views.detail, name="detail"),
+  url(r'^(?P<question_id>\d+)/results/$', views.results, name="results"),
+  url(r'^(?P<question_id>\d+)/vote/$', views.vote, name="vote"),
+  url(r'^(?P<nama>\w+)/(?P<nim>\d+)/mhs/$', views.mhs, name="mhs"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Create your views here.
+
 def index(request):
     return HttpResponse("Hello world! You are in polls's index")
 
-# Create your views here.
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+def results(request, question_id):
+    response = "You're looking at result of question %s."
+    return HttpResponse( response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting at question %s" % question_id)
+
+def mhs(request, nama, nim):
+    return HttpResponse("nama : %s<br>nim : %s" % (nama, nim))
