a345592c75c8f6987b0d3a42f90e6a1e7e4d77d7
supriadi-yusuf <supriadi96@gmail.com>
2019-03-13 12:49:00 +0700

    use url name instead of hardcode url

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name="index"),
-  url(r'^(?P<question_id>\d+)/$', views.detail, name="detail"),
+  url(r'^specifics/(?P<question_id>\d+)/$', views.detail, name="detail"),
   url(r'^(?P<question_id>\d+)/results/$', views.results, name="results"),
   url(r'^(?P<question_id>\d+)/vote/$', views.vote, name="vote"),
   url(r'^(?P<nama>\w+)/(?P<nim>\d+)/mhs/$', views.mhs, name="mhs"),
