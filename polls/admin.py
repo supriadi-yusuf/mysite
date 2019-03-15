b2c3e591c7ad5758d89b555fbef15510b8177676
supriadi-yusuf <supriadi96@gmail.com>
2019-03-15 10:13:04 +0700

    change field order for Question in admin page

--- polls/admin.py
+++ polls/admin.py
@@ -4,5 +4,8 @@ from . import models
 
 # Register your models here.
 
-admin.site.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ['pub_date','question_text']
+
+admin.site.register(models.Question, QuestionAdmin)
 admin.site.register(models.Choice)
